d80c0b1169553d684d8fae262521e9d77ae7625c
Jian Lee <ramosjianlee@gmail.com>
2026-02-12 23:20:24 +0800

    feat: implement contact management functionality with CRUD operations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,81 @@
+import ContactList from "./components/ContactList";
+import NavBar from "./components/NavBar";
+import axios from "axios";
 import "./index.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState();
+
+  useEffect(() => {
+    getAllContacts();
+  }, []);
+
+  const getAllContacts = async () => {
+    try {
+      const allContacts = await axios.get(`http://localhost:5000/contacts`);
+      setContacts(allContacts.data.data);
+    } catch (err) {
+      console.error("[frontend]: Error fetching all contacts", err.message);
+    }
+  };
+
+  const createContact = async (
+    first_name,
+    last_name,
+    contact_number,
+    email_address,
+  ) => {
+    try {
+      const newContact = await axios.post(`http://localhost:5000/contacts`, {
+        first_name,
+        last_name,
+        contact_number,
+        email_address,
+      });
+      setContacts([...contacts, newContact.data.data]);
+    } catch (err) {
+      console.error("[frontend]: Error creating new contact!", err.message);
+    }
+  };
+
+  const updateContact = async (
+    contact_id,
+    first_name,
+    last_name,
+    contact_number,
+    email_address,
+  ) => {
+    try {
+      const updatedContact = await axios.put(
+        `http://localhost:5000/contacts/${contact_id}`,
+        { contact_id, first_name, last_name, contact_number, email_address },
+      );
+
+      setContacts(
+        contacts.map((contacts) =>
+          contacts.id === contact_id ? updatedContact.data.data : contacts,
+        ),
+      );
+    } catch (err) {
+      console.error("[frontend]: Error updating contact!", err.message);
+    }
+  };
+
+  const deleteContact = async (contact_id) => {
+    try {
+      await axios.delete(`http://localhost:5000/contacts/${contact_id}`);
+      setContacts(contacts.filter((contacts) => contacts.id !== contact_id));
+    } catch (err) {
+      console.error("[frontend]: Error deleting contacts!", err.message);
+    }
+  };
+
   return (
     <>
       <div>
-        <h1>Hello World!</h1>
+        <NavBar />
+        <ContactList />
       </div>
     </>
   );
