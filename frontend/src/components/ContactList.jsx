096e9d5d99da062cd6fb8ae9230133cb8f05ea5d
Jian Lee <ramosjianlee@gmail.com>
2026-02-12 23:20:36 +0800

    feat: add ContactList component to display contacts

---
+++ frontend/src/components/ContactList.jsx
@@ -0,0 +1,14 @@
+import ContactItem from "./ContactItem";
+
+function ContactList() {
+  return (
+    <>
+      <div className="w-screen h-screen pt-15 bg-gray-300">
+        <h1>Contact List</h1>
+        <ContactItem />
+      </div>
+    </>
+  );
+}
+
+export default ContactList;
