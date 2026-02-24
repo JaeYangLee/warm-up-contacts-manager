988832463adb90484d0000a000717415687d84de
Jian Lee <ramosjianlee@gmail.com>
2026-02-24 20:48:42 +0800

    feat: enhance NavBar component with search functionality and improved button layout

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 function NavBar({ contact, onAdd }) {
+  const [search, setSearch] = useState("");
+
+  const handleReset = () => {
+    setSearch("");
+  };
+
   return (
     <>
-      <div className="w-screen h-14 fixed top-0 bg-gray-500 flex flex-row items-center justify-center">
+      <div className="fixed top-0 flex flex-row items-center justify-center w-screen gap-2 px-4 bg-gray-500 h-14">
+        <section>
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Search..."
+            className="px-2 border rounded shadow-[2px_2px_0px_0px]"
+          />
+        </section>
         <section className="flex flex-row items-center justify-center gap-2">
-          <button className="px-2 border rounded shadow-[2px_2px_0px_0px]">
-            Add
+          <button
+            onClick={onAdd}
+            className="px-2 border rounded-full shadow-[2px_2px_0px_0px]"
+          >
+            +
           </button>
-          <button className="px-2 border rounded shadow-[2px_2px_0px_0px]">
+          <button
+            onClick={handleReset}
+            className="px-2 border rounded shadow-[2px_2px_0px_0px]"
+          >
             Clear
           </button>
         </section>
