8458893ba9e1318005ff4df7fe41d9e3138d56a2
Jian Lee <ramosjianlee@gmail.com>
2026-02-24 20:40:38 +0800

    feat: add react-icons dependency and enhance ContactItem and ContactList components with improved layout and functionality

--- frontend/src/components/ContactItem.jsx
+++ frontend/src/components/ContactItem.jsx
@@ -1,10 +1,25 @@
+import { BsFillPersonFill } from "react-icons/bs";
+
 function ContactItem({ contact, onUpdate, onDelete }) {
   return (
     <>
-      <li>
-        <p>{contact.first_name}</p>
-        <p>{contact.contact_number}</p>
-        <p>{contact.email_address}</p>
+      <li className="flex flex-row items-center justify-between px-2 text-sm gap-2 py-2 border-b border-t">
+        <section className="flex flex-row items-center justify-center">
+          <BsFillPersonFill className="size-12 border rounded-full p-1" />
+        </section>
+        <section className="w-full flex flex-col">
+          <p>{`${contact.first_name} ${contact.last_name}`}</p>
+          <p>{contact.contact_number}</p>
+          <p>{contact.email_address}</p>
+        </section>
+        <section className="flex flex-col gap-2">
+          <button className="border rounded px-2 shadow-[2px_2px_0px_0px]">
+            Edit
+          </button>
+          <button className="border rounded px-2 shadow-[2px_2px_0px_0px]">
+            Delete
+          </button>
+        </section>
       </li>
     </>
   );
--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -3,13 +3,13 @@ import ContactItem from "./ContactItem";
 function ContactList({ contacts = [], onUpdate, onDelete }) {
   return (
     <>
-      <ul>
+      <ul className="w-screen h-screen pt-14">
         {contacts.length === 0 ? (
           <h1>No contacts...</h1>
         ) : (
           contacts.map((contacts) => (
             <ContactItem
-              key={contacts.id}
+              key={contacts.contact_id}
               contact={contacts}
               onUpdate={onUpdate}
               onDelete={onDelete}
